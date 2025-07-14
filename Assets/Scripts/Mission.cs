3ba1066ef6c5837f8cf58f7f86887f9cdd96b6bc
VanPhamNe <119727678+VanPhamNe@users.noreply.github.com>
2025-07-14 15:25:57 +0700

    mission

---
+++ Assets/Scripts/Mission.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Mission : ScriptableObject
+{
+    public string missionName; // Ten nhiem vu
+    public string missionDescription; // Mo ta nhiem vu
+    public abstract void StartMission(); // Bat dau nhiem vu
+    public abstract bool IsMissionComplete(); // Kiem tra xem nhiem vu da hoan thanh chua
+    public virtual void UpdateMission()
+    {
+        // Phuong thuc co the duoc ghi de trong cac lop ke thua
+        // De cap nhat trang thai nhiem vu neu can thiet
+    }
+}
---
+++ Assets/Scripts/MissionManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionManager : MonoBehaviour
+{
+    public static MissionManager Instance;
+    public Mission currentMission; // Nhiem vu hien tai
+    private void Awake()
+    {
+        Instance = this;
+    }
+    private void Start()
+    {
+        if (currentMission != null)
+        {
+            StartMission(); // Bat dau nhiem vu neu co
+        }
+    }
+    private void Update()
+    {
+        currentMission?.UpdateMission(); // Cap nhat nhiem vu hien tai neu co
+    }
+    private void StartMission() => currentMission.StartMission(); // Bat dau nhiem vu hien tai neu co
+    public bool MissionComplete() => currentMission.IsMissionComplete(); // Kiem tra xem nhiem vu hien tai da hoan thanh chua
+    
+}
